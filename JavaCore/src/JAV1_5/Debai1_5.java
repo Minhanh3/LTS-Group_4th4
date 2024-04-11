ccb2ce50b542e0d248ae3ca38ab4f8530f4e7cbe
ducManh3 <Maylua9f@gmail.com>
2024-04-11 21:20:42 +0700

    up lan 2

--- JavaCore/src/JAV1_5/Debai1_5.java
+++ JavaCore/src/JAV1_5/Debai1_5.java
@@ -15,6 +15,13 @@ Sau đó dùng SimpleDateFormat hỗ trợ in ra thời gian theo định dạng
 new SimpleDateFormat("dd-MM-yyyy - HH:mm:ss");
 */
 /*
-
+Nhập vào một đoạn văn, bạn cần phải kiểm tra xem đoạn văn ấy là tiếng Anh-Anh
+hay tiếng Anh-Mỹ. Để làm được việc này bạn cần đếm số chữ “t” và chữ “s” trong đoạn văn ấy.
+Giả sử: Chữ 's' nhiều hơn chữ 't' => tiếng Anh-Anh, ngược lại => tiếng Anh-Mỹ
+Đầu vào: Nhập 1 chuỗi
+Đầu ra: Hiển thị “A-A” nếu có nhiều chữ “s” hơn và in ra “A-M” nếu có nhiều chữ “t” hơn.
+Nếu số lượng chữ “t” bằng chữ “s” in ra “A-A”
+VD: "School is the bored place. So you should go to the game centre. It is funny and very
+good for relaxing." => A-A
 */
 }
--- JavaCore/src/JAV1_5/JAV1_5_14_Display_date_time_now_inCalendar.java
+++ JavaCore/src/JAV1_5/JAV1_5_14_Display_date_time_now_inCalendar.java
@@ -1,13 +1,17 @@
 package JAV1_5;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Calendar;
 
 public class JAV1_5_14_Display_date_time_now_inCalendar {
     public static void main(String[] args) {
-        Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat DateFormat = new SimpleDateFormat("dd-MM-yyy - HH:mm:ss");
-        String format = DateFormat.format(calendar.getTime());
-        System.out.println("Date: " + format);
+//        Calendar calendar = Calendar.getInstance();
+//        SimpleDateFormat DateFormat = new SimpleDateFormat("dd-MM-yyy - HH:mm:ss");
+//        String format = DateFormat.format(calendar.getTime());
+//        System.out.println("Date: " + format);
+        System.out.println(LocalDateTime.now());
     }
 }
